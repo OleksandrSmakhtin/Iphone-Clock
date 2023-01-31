d3b83dff4cdeeb7433978d5580c300be7a29092c
Oleksandr Smakhtin <skiper.smaxtin@icloud.com>
2023-01-31 15:36:15 +0200

    Initial Commit

---
+++ Iphone Clock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iphone Clock
+//
+//  Created by Oleksandr Smakhtin on 31.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
